6b1cf2f2ef12911dc1c727f2458e89ad5d36ccd3
Ayushss-123 <ayushsrivastav323@gmail.com>
2023-02-22 15:11:24 +0000

    Locofy commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import {
   useNavigationType,
   useLocation,
 } from "react-router-dom";
-import { MyRideBecomeADriverPopu } from "./my-ride-become-a-driver-popu";
+import Technologies from "../test123/pages/name 123/section 1/Technologies";
+import MAINPAGE from "./public/pages/MAINPAGE";
+import SignUp from "../test123/pages/SignUp";
+import Login from "../test123/pages/Login";
+import ContactUs from "../test123/pages/ContactUs";
+import Component from "../test123/pages/Component";
+import FrameComponent from "./public/pages/FrameComponent";
 import { useEffect } from "react";
 
 function App() {
@@ -16,15 +22,38 @@ function App() {
     if (action !== "POP") {
       window.scrollTo(0, 0);
     }
-  }, [action]);
+  }, [action, pathname]);
 
   useEffect(() => {
     let title = "";
     let metaDescription = "";
 
-    //TODO: Update meta titles and descriptions below
     switch (pathname) {
       case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/main-page":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/sign-up":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/login":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/contact-us":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/2":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/frame-782":
         title = "";
         metaDescription = "";
         break;
@@ -46,7 +75,19 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<MyRideBecomeADriverPopu />} />
+      <Route path="/" element={<Technologies />} />
+
+      <Route path="/main-page" element={<MAINPAGE />} />
+
+      <Route path="/sign-up" element={<SignUp />} />
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/contact-us" element={<ContactUs />} />
+
+      <Route path="/2" element={<Component />} />
+
+      <Route path="/frame-782" element={<FrameComponent />} />
     </Routes>
   );
 }
